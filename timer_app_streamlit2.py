44e041b66428fc0b5f9585b0695882141078ad7a
PanSaBulsa <woodcreepers023@gmail.com>
2026-02-19 11:52:36 +0800

    Update timer_app_streamlit2.py

--- timer_app_streamlit2.py
+++ timer_app_streamlit2.py
@@ -357,7 +357,7 @@ if st.session_state.page == "world":
 
     with left_btn:
         if not st.session_state.auth:
-            if st.button("🔐 Login to edit timers"):
+            if st.button("🔐 Admin Login"):
                 goto("login")
         else:
             if st.button("🛠️ Manage / Edit"):
@@ -494,4 +494,5 @@ elif st.session_state.page == "history":
                 st.info("No edits yet.")
         else:
             st.info("No edit history yet.")
+
 
